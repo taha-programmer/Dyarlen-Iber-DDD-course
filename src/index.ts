4300e4e3ca643c09d24163fbc85e724cb8f9de4c
Taha <taha@marketer.ly>
2023-04-12 09:22:18 +0200

    Finally I applied the Hexagonal Arch in Code :)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,49 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import csv from 'csv';
+
+type UserData = {
+  name: string;
+  email: string;
+  dateOfBirth: Date;
+};
+
+// outgoing/driven port -- aka Gateway -- Uncle Bob call it: Use Case Output port
+interface IExportUserData {
+  export(userData: UserData): void;
+}
+
+// outgoing Adapters
+class ExportUserToPDF implements IExportUserData {
+  export(userData: UserData): void {
+    // throw new Error('Method not implemented.');
+    /* pdf package --- here we can use any package we want to deal with infrastructure...
+       because now we're in the Adapter :)
+    */
+  }
+}
+class ExportUserToCSV implements IExportUserData {
+  export(userData: UserData): void {
+    /* csv package --- here we can use any package we want to deal with infrastructure...
+       because now we're in the Adapter :)
+    */
+  }
+}
+
+// The code in the useCase is an Application Layer Concern, it's inside the Hexagon
+class ExportUserUseCase {
+  constructor(private exportUserData: IExportUserData) {}
+  execute(userData: UserData): void {
+    //csv.export,, if we do this, then we're depeding on Infrastructure concern, instead we'll do:
+    this.exportUserData.export(userData);
+  }
+}
+
+// maybe the composition root?
+const exportUserToPDF = new ExportUserToPDF(); // create an instance of our Gateway Adapter
+const exportUser = new ExportUserUseCase(exportUserToPDF); // Inject the Adapter in the useCase
+
+exportUser.execute({
+  name: 'Hatem',
+  email: 'hatem@google.com',
+  dateOfBirth: new Date(1983, 4, 12),
+});
